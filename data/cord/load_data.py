97260bb4849d4179f1d950029ef24d63c14f89f0
Ubuntu <ubuntu@ip-172-31-0-219.eu-west-1.compute.internal>
2020-06-01 18:48:15 +0000

    Fixed loading into psql

--- data/cord/load_data.py
+++ data/cord/load_data.py
@@ -4,14 +4,17 @@ import os
 from pprint import pprint
 import sys
 
+import boto3
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from db_api.models import Article
 from db_api.database import global_init, session_scope
 
 
-def main(start_date):
+def main(start_date, s3=False, psql=True):
     global_init()
+    s3_client = boto3.client('s3')
     with open("metadata.csv", newline="") as csvfile, session_scope() as sess:
         reader = csv.DictReader(csvfile)
         ids_added = set()
@@ -27,27 +30,45 @@ def main(start_date):
             article.id = cord_id
             article.version = "v1"
             article.source = "CORD"
-            article.title = row["title"]
-            publish_date = row["publish_time"]
+            article.journal = row.get("journal", "")
+            article.article_type = "preprint"
+            article.title = row.get("title", "")
+            publish_date = row.get("publish_time", "")
             publish_date_parsed = None
             try:
                 publish_date_parsed = date.fromisoformat(publish_date)
                 article.publication_date = publish_date_parsed
             except ValueError:
-                pass
+                article.publication_date = ""
             article.update_date = date.today()
             article.modified_date = datetime.now()
-            article.link = row["url"]
+            article.link = row.get("url", "")
             pmid = row["pubmed_id"]
             try:
                 article.pmid = int(pmid)
             except ValueError:
-                pass
-            article.doid = row["doi"]
-            article.summary = row["abstract"]
+                article.pmid = 0
+            article.doid = row.get("doi", "")
+            article.summary = row.get("abstract", "")
+            article.full_text = ""
             article.authors = [x.strip() for x in row["authors"].split(";")]
+            article.affiliations = []
+            article.language = ""
+            article.keywords = []
+            article.references = []
             if publish_date_parsed and publish_date_parsed > start_date:
-                sess.add(article)
+                if s3:
+                    fname = f"{article.id}.tsv"
+                    with open(fname, "w") as f:
+                        f.write(article.id)
+                        f.write(os.linesep)
+                        f.write(article.title)
+                        f.write(os.linesep)
+                        f.write(article.summary)
+                    s3_client.upload_file(fname, "kendra-kbase-ajs-aws", fname)
+                    os.remove(fname)
+                if psql:
+                    sess.add(article)
 
 
 if __name__ == "__main__":
