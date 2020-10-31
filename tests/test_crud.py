88b00a5385f013e9da8b824fbc4fb494de12312f
Alexander Junge <alexander.junge@gmail.com>
2020-10-31 19:37:13 +0100

    Import crud in test_crud

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,4 +1,11 @@
+import os
+import sys
+
 from pytest import approx
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from db_api.crud import precision_recall_fscore
 
 
 def test_precision_recall_f1score():
