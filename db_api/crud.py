63ac90feea0c111c78da55ad756bcafcbb6e6dd0
Alexander Junge <alexander.junge@gmail.com>
2020-11-05 20:15:15 +0100

    Add CRUD for NER evaluation

--- db_api/crud.py
+++ db_api/crud.py
@@ -254,3 +254,39 @@ def precision_recall_fscore(
         (precision * recall) / (((beta ** 2) * precision) + recall + 1e-100)
     )
     return precision, recall, fscore, tp, fp, fn
+
+
+def create_ner_evaluation(
+    db: Session, ner_evaluation: schemas.NEREvaluationCreate
+) -> models.NEREvaluation:
+    db_ner_evaluation = models.NEREvaluation(**ner_evaluation.dict())
+    db.add(db_ner_evaluation)
+    db.commit()
+    db.refresh(db_ner_evaluation)
+    return db_ner_evaluation
+
+
+def get_ner_evaluation(db: Session, ner_evaluation_id: int) -> models.NEREvaluation:
+    return (
+        db.query(models.NEREvaluation)
+        .filter(models.NEREvaluation.id == ner_evaluation_id)
+        .first()
+    )
+
+
+def get_ner_evaluations(
+    db: Session, skip: int = 0, limit: int = 100
+) -> Iterable[models.NEREvaluation]:
+    return db.query(models.NEREvaluation).offset(skip).limit(limit).all()
+
+
+def update_ner_evaluation(
+    db: Session, ner_evaluation: schemas.NEREvaluationUpdate
+) -> models.NEREvaluation:
+    new_ner_evaluation = models.NEREvaluation(**ner_evaluation.dict())
+    old_ner_evaluation = get_ner_evaluation(db, ner_evaluation.id)
+    db.delete(old_ner_evaluation)
+    db.add(new_ner_evaluation)
+    db.commit()
+    db.refresh(new_ner_evaluation)
+    return new_ner_evaluation
