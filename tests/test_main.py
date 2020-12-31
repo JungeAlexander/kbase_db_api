0f832d2cba928c77939a1bb62734db43351b3b79
Alexander Junge <alexander.junge@gmail.com>
2020-12-31 10:20:45 +0100

    Add test for user create, auth, access flow

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,4 +22,19 @@ def test_create_auth_user():
     email = random_email()
     username = random_lower_string()
     password = random_lower_string()
-    assert False
+    # create random user
+    user_dict = {"email": email, "username": username, "password": password}
+    r = client.post("/users/", json=user_dict)
+    assert r.status_code == 200, r.text
+    # get token
+    data_dict = {"username": username, "password": password}
+    r = client.post("/token", data=data_dict)
+    assert r.status_code == 200, r.text
+    j = r.json()
+    assert "access_token" in j, j
+    # request user info
+    header_dict = {"Authorization": f"{j['token_type']} {j['access_token']}"}
+    r = client.get("/me", headers=header_dict)
+    j = r.json()
+    assert j["email"] == email, j
+    assert j["username"] == username, j
